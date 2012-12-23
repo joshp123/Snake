762a872b3e5b904094d1797eaef526f8d712bf59
Josh Palmer <joshpalmer123@gmail.com>
2012-12-23 18:03:15 +0000

    walls resize properly; started u/d/l/r logic

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -73,15 +73,22 @@ namespace Snake
             }
 
         }
-        
+
+        internal static Coordinate food = new Coordinate(10,10);
+        internal static List<Coordinate> snake = new List<Coordinate>();
+        internal static List<Coordinate> walls = new List<Coordinate>();
+        internal static Coordinate screen_size = new Coordinate(79, 40);
+
         static void Main(string[] args)
         {
-            List<Coordinate> snake = new List<Coordinate>();
+            // List<Coordinate> snake = new List<Coordinate>();
             snake.Add(new Coordinate(1, 1));
             // initialize snake with length of 1 at 1,1 (closest poss point to origin)
 
-            List<Coordinate> walls = InititialiseWalls(79, 40);
-            Coordinate food = new Coordinate(10,10); // make random food constructor soon
+            walls = InititialiseWalls(screen_size);
+            
+            food = new Coordinate(10,10); // make random food constructor soon. make it an object i guess
+
             DrawScreen(walls, snake, food);
 
             // start the snake moving
@@ -90,7 +97,7 @@ namespace Snake
             int time = 0;
             // BEWARE OF INT32 WRAPAROUND BUGS HERE LOL
 
-            while (true)
+            while (time < 30)
             // move snake perpetually unless we break out of loop
             {
                 DrawScreen(walls, snake, food);
@@ -99,14 +106,14 @@ namespace Snake
                 // if hit wall, break
 
                 // if hit food, eat food, length++ (i.e. new coord at last point) snake.add(snake[snake.length-1]) after rest of snake moves
-                
+
                 // move rest of snake
                 for (int i = snake.Count - 1; i > 1; i--)
                 {
                     // loop backwards along snake moving each coord forward
                     snake[i] = snake[i - 1];
-			    }
-                
+                }
+
                 System.Threading.Thread.Sleep(500);
                 time++;
                 // each time unit is 0.5s
@@ -115,27 +122,33 @@ namespace Snake
 
         }
 
-        static List<Coordinate> InititialiseWalls(int xwidth, int ywidth)
+        static List<Coordinate> InititialiseWalls(Coordinate screen_size)
         {
+            int xwidth = screen_size.x;
+            int ywidth = screen_size.y;
+
             // set up the walls on lines x = 0; y = 0; x = 82; y = 82; (to create 80x80 playing space)
             List<Coordinate> walls = new List<Coordinate>();
-            for (int i = 0; i < xwidth; i++)
+            for (int i = 1; i < ywidth; i++)
             {
-                walls.Add(new Coordinate(i, 0));
+                walls.Add(new Coordinate(0, i));
                 // x = 0;
                 walls.Add(new Coordinate(xwidth, i));
                 // x = width
             }
 
-            for (int i = 0; i < ywidth; i++)
+            for (int i = 1; i < xwidth; i++)
             {
-                walls.Add(new Coordinate(0, i));
+                walls.Add(new Coordinate(i, 0));
                 // y = 0;
                 
                 walls.Add(new Coordinate(i, ywidth));
                 // y = width
 
             }
+            walls.Add(new Coordinate(0, 0));
+            walls.Add(new Coordinate(0, ywidth));
+            walls.Add(new Coordinate(xwidth, 0));
             walls.Add(new Coordinate(xwidth, ywidth));
             return walls;
             
@@ -143,6 +156,7 @@ namespace Snake
 
         static void DrawScreen(List<Coordinate> walls, List<Coordinate> snake, Coordinate food)
         {
+            Console.SetWindowSize(screen_size.x + 1, screen_size.y + 2);
             Console.Clear();
             // draw walls (u+2588)
             foreach (var point in walls)
@@ -160,7 +174,7 @@ namespace Snake
             Console.SetCursorPosition(food.x, food.y);
             Console.Write("o");
             // get the cursor the fuck out
-            Console.SetCursorPosition(0, 80);
+            Console.SetCursorPosition(0, screen_size.y + 1);
         }
 
     }
